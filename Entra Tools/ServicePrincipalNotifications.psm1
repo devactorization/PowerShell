b033836bb3d6cbce0ae3609add79f58b80bd3020
Don Morgan <59622685+mister-dj@users.noreply.github.com>
2024-09-12 11:28:46 -0500

    Updating output

--- Entra Tools/ServicePrincipalNotifications.psm1
+++ Entra Tools/ServicePrincipalNotifications.psm1
@@ -101,7 +101,7 @@ function Get-ServicePrincipalNotifications {
         Write-Host -ForegroundColor Green "Getting service principals, this may take a while..."
         $FilteredSPs = GetServicePrincipals
 
-        $FilteredSPs # | Format-Table DisplayName,Id,NotificationEmailAddresses,ServicePrincipalType
+        Write-Output $FilteredSPs # | Format-Table DisplayName,Id,NotificationEmailAddresses,ServicePrincipalType
     }
 
     elseif($Update){
