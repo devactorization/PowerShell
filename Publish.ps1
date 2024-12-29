c44cc149d547dd4d735c7ecdc063b9d90c57da0c
Don Morgan <donm@donmorgan.net>
2024-12-29 15:41:56 -0600

    Updating workflow

--- Publish.ps1
+++ Publish.ps1
@@ -2,5 +2,10 @@ param(
     [string]$TargetFolder
 )
 
+Write-Verbose "Publishing module at path: $TargetFolder"
+ls
+Write-Verbose "Testing path..."
+Test-Path $TargetFolder
+
 $ModulePath = "$PSScriptRoot\$TargetFolder"
 Publish-PSResource -Path $ModulePath -ApiKey $Env:APIKEY
