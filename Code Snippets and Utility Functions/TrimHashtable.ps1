c7781f57f9b0f3dfea597197d5d3298e0b40892a
Don Morgan <donm@donmorgan.net>
2025-03-05 21:36:28 -0600

    Updated tools and snippets

---
+++ Code Snippets and Utility Functions/TrimHashtable.ps1
@@ -0,0 +1,19 @@
+function TrimHashTable{
+    <#
+    .SYNOPSIS
+        Removes empty string or null value keys from a hashtable
+    #>
+    param(
+        [Parameter(Mandatory = $true)]
+        [object]$Hashtable
+    )
+
+    $CleanHashtable = @{}
+    $Keys = $Hashtable.Keys
+    foreach ($Key in $Keys){
+        if(($Hashtable.$Key -ne "") -and ($null -ne $Hashtable.$Key)){
+            $CleanHashtable.Add($Key,$Hashtable.$Key)
+        }
+    }
+    return $CleanHashtable
+}
